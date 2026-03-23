d2a0d7b175805027e3969fb81331edcb5c364a7e
manojkumar-maddy <sanniboina.manojkumar@gmail.com>
2026-03-23 17:55:19 +0530

    add

--- 01-birthday-reminder/final/src/data.js
+++ 01-birthday-reminder/final/src/data.js
@@ -34,4 +34,11 @@ export default [
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
   },
+  {
+    id: 6,
+    name: 'manoj',
+    age: 29,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+  },
 ];
